9c3448b9ff0902a6d36574a97e8e38378b41113e
Georgi Daleev <gdaleev2346@gmail.com>
2024-05-02 13:36:18 +0300

    Added functionality to buttons

--- note-managment/src/app/app.component.ts
+++ note-managment/src/app/app.component.ts
@@ -1,25 +1,56 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NotesComponent } from './notes/notes.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NotesComponent, FormsModule],
+  imports: [RouterOutlet, NotesComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  @ViewChild('notesComponent') notesComponent!: NotesComponent;
   title = '';
   content = '';
   notesArray: { title: string; content: string }[] = [];
+  editModeOn = false;
+  index = 0;
+
+  ngAfterViewInit() {
+    this.index = this.notesComponent.index; // Move index assignment to ngAfterViewInit
+    this.editModeOn = this.notesComponent.editModeOn;
+  }
 
   addNote() {
-    this.notesArray.push({ title: this.title, content: this.content });
-    console.log(this.notesArray);
+    console.log('editModeOn:', this.editModeOn);
+    if (this.editModeOn) {
+      console.log('Calling updateNote');
+      this.updateNote();
+    } else {
+      this.notesArray.push({ title: this.title, content: this.content });
+      // console.log(this.notesArray);
+      this.title = '';
+      this.content = '';
+      this.notesComponent.listItemClicked = false;
+    }
+  }
 
+  updateNote() {
+    this.notesArray[this.index] = { title: this.title, content: this.content }
     this.title = '';
     this.content = '';
+    this.notesComponent.listItemClicked = false;
+    this.editModeOn = false;
   }
+
+  onChildValuesEvent(values: { titleValue: string; contentValue: string; editMode?: boolean; index?: number }) {
+    this.title = values.titleValue;
+    this.content = values.contentValue;
+    this.editModeOn = !!values.editMode;
+    this.index = values.index || 0;
+  }
+
 }
--- note-managment/src/app/notes/notes.component.ts
+++ note-managment/src/app/notes/notes.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-notes',
@@ -10,4 +10,26 @@ import { Component, Input } from '@angular/core';
 })
 export class NotesComponent {
   @Input() notesArray: { title: string; content: string }[] = [];
+  @Input() editModeOn = false;
+  @Input() index = 0;
+  @Output() childValuesEvent = new EventEmitter<{ titleValue: string, contentValue: string }>();
+  listItemClicked = false;
+
+  showButtons() {
+    this.listItemClicked = true
+  }
+
+  deleteNote(index: number) {
+    this.notesArray.splice(index, 1)
+    this.listItemClicked = false;
+  }
+
+  editNote(index: number) {
+    const selectedNote = this.notesArray[index];
+    const titleValue = selectedNote.title;
+    const contentValue = selectedNote.content;
+
+    this.childValuesEvent.emit({ titleValue, contentValue, editMode: true, index } as { titleValue: string; contentValue: string; editMode?: boolean, index: number });
+  }
+
 }
