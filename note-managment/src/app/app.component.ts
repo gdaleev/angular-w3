11061d3cedfbb3db79930244dc15e5278cb13108
Georgi Daleev <gdaleev2346@gmail.com>
2024-05-02 11:26:23 +0300

    Added logic for note addition

--- note-managment/src/app/app.component.ts
+++ note-managment/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NotesComponent } from './notes/notes.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NotesComponent],
+  imports: [RouterOutlet, NotesComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  
+  title = '';
+  content = '';
+  notesArray: { title: string; content: string }[] = [];
+
+  addNote() {
+    this.notesArray.push({ title: this.title, content: this.content });
+    console.log(this.notesArray);
+
+    this.title = '';
+    this.content = '';
+  }
 }
--- note-managment/src/app/notes/notes.component.ts
+++ note-managment/src/app/notes/notes.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-notes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './notes.component.html',
-  styleUrl: './notes.component.css'
+  styleUrl: './notes.component.css',
 })
 export class NotesComponent {
-
+  @Input() notesArray: { title: string; content: string }[] = [];
 }
